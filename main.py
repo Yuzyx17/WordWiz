1908afd44ed538e46c2199c946b32026602fb4fe
EunicePMV <villanueva.eunice.m@gmail.com>
2023-05-15 18:56:22 +0800

    added text, bug in interface

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ if __name__ == "__main__":
     init_game()
     # sampleMM()
     # sampleCB("glass", "spadeglass")
- 
--- src/utils/text.py
+++ src/utils/text.py
@@ -2,6 +2,29 @@ import pygame as pg
 from pygame.math import Vector2 as vec2
 from src.constants import *
 
+# class TextRenderer(pg.sprite.Sprite):
+#     def __init__(self, size: vec2):
+#         pg.sprite.Sprite.__init__(self)
+#         self.text = ""
+#         self.change = False
+#         self.image = pg.Surface(size)
+#         self.image.set_alpha(100)
+#         self.rect = self.image.get_rect()
+
+#     def update(self):
+#         if self.change:
+#             text = pixelfont_sm.render(self.text, True, WHITE)
+#             text_rect = text.get_rect()
+#             text_rect.centerx = self.rect.w//2
+#             text_rect.centery = self.rect.h//2
+#             self.image.blit(text, text_rect)
+#             self.change = False
+    
+#     def change_text(self, text):
+#         self.text = text
+#         self.change = True
+
+
 class TextRenderer(pg.sprite.Sprite):
     def __init__(self, size: vec2, color = BLACK):
         pg.sprite.Sprite.__init__(self)
@@ -9,10 +32,18 @@ class TextRenderer(pg.sprite.Sprite):
         self.change = False
         self.image = pg.Surface(size)
         self.rect = self.image.get_rect()
+        self.image.fill(color)
+        self.image.set_colorkey(color)
+        self.color = color
+        self.setVisible = True
 
-    def update(self):
+    def update(self, color=(15, 15, 15)):
+        if not self.setVisible:
+            self.image.fill(self.color)
+        # print(self.text)
         if self.change:
-            text = pixelfont_sm.render(self.text, True, BLACK)
+            self.image.fill(self.color)
+            text = pixelfont_sm.render(self.text, True, color)
             text_rect = text.get_rect()
             text_rect.centerx = self.rect.w//2
             text_rect.centery = self.rect.h//2
@@ -22,3 +53,10 @@ class TextRenderer(pg.sprite.Sprite):
     def change_text(self, text):
         self.text = text
         self.change = True
+
+# self.font = pygame.font.SysFont("Arial", size)
+# self.textSurf = self.font.render(text, 1, color)
+# self.image = pygame.Surface((width, height))
+# W = self.textSurf.get_width()
+# H = self.textSurf.get_height()
+# self.image.blit(self.textSurf, [width/2 - W/2, height/2 - H/2])
