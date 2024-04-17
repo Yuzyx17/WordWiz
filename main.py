1ec009526bc1dc1a8f21c7db8da490c79ac4f375
Yuzyx17 <braitboat@gmail.com>
2024-04-17 10:02:02 +0800

    added init py

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+import sys
+import os
+
+def resource_path(relative_path):
+    try:
+    # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+sys.path.insert(0, "src/ai")
+sys.path.insert(0, "src/core")
+sys.path.insert(0, "src/game")
+sys.path.insert(0, "src/utils")
+sys.path.insert(0, "src")
+resource_path("src/ai")
+resource_path("src/core")
+resource_path("src/game")
+resource_path("src/utils")
+resource_path("src")
+resource_path("assets/data/oldtrie.dat")
+resource_path("assets/data/trie.dat")
+resource_path("assets/data/dictionary.txt")
+resource_path("assets/data/old-dictionary.txt")
+resource_path("assets/data/sm-dictionary.txt")
+resource_path("assets/font/pixelfont.ttf")
+resource_path("assets/sfx/button.wav")
+resource_path("assets/sfx/gamefx.wav")
+resource_path("assets/sfx/letter.wav")
+
+import sys
+
+
 if __name__ == "__main__":
     from src.game.wordwiz import init_game
-    from test.test import sampleCB, sampleMM
     init_game()
     # sampleMM()
     # sampleCB("glass", "spadeglass")
+
--- src/game/player.py
+++ src/game/player.py
@@ -123,30 +123,4 @@ class Player():
 
     def mastermind(self):
         self.board.spell = self.state.can_spell_code() 
-        # letter: Letter
-        # for letter in self.board.letter_pool:         #for letters in the pool
-        #     if not letter.clicked and letter not in self.board.letter_used:
-        #         if letter.click(self.board.spell and self.board.click):
-        #             #State update
-        #             #Gets the index of the letter clicked from pool
-        #             attempted_index = self.board.letter_pool.sprites().index(letter)        
-        #             #Gets the FIRST empty slot from self.state.guesses[self.attempt]
-        #             attempted_index = self.state.spell_code(attempted_index, letter.letter)
-        #             #Translate the position of the letter to the empty slot
-        #             letter.translate(vec2(tilesize.x*attempted_index, 100+(self.state.attempt*tilesize.y)))
-        #             self.board.letter_used.add(letter)   
-        #             self.board.click = False
-        #             break
-        #             # letter.emulated_click()
-
-        # for letter in self.board.letter_used:         #for letters used
-        #     if not letter.clicked:
-        #         if letter.click(self.board.click):
-        #             #State update
-        #             attempted_index = self.board.letter_pool.sprites().index(letter)
-        #             letter.translate(vec2(tilesize.x*attempted_index, 25))
-        #             self.state.undo_code(attempted_index, letter.letter)
-        #             self.board.letter_used.remove(letter) 
-        #             self.board.click = False
-        #             break
         
